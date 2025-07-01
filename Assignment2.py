d08495a3a3e7e66b94259c0179e82b39dab90b61
swarn9 <139808767+swarn9@users.noreply.github.com>
2025-07-01 12:40:00 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,30 @@
+############_____________Task 1______________#######################
+
+a = int(input("Enter a number :"))
+if a %2==0:
+    print(f'{a} is an even number')
+else:
+    print(f'{a} is an odd number')
+    
+    
+    
+############_____________Task 2______________#######################
+
+
+
+# _____ For Loop__________________
+sum =0
+for i in range(1,51):
+    sum=sum+i
+print(f'The sum of numbers from 1 to 50 is :{sum}')
+
+
+# ____ while Loop________
+sum =0
+i=1
+while i<=50:
+    sum=sum+i
+    i+= 1
+print(f'The sum of numbers from 1 to 50 is :{sum}')
+
+
